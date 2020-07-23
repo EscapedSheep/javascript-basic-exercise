c5f5f2afd6b5076dc11dab6e99db38a687b3cc22
EscapedSheep <sfgmdf001@126.com>
2020-07-23 18:18:09 +0800

    [maida Ge] Completed exercise_03

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -7,7 +7,15 @@ export function range(start, end) {
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (start == end) { return []; }
+
+  const dirction = end - start;
+  const step = dirction > 0 ? 1 : -1;
+  let result = [];
+  for (let i = start; dirction > 0 ? i < end : i > end; i+=step) {
+    result.push(i);
+  }
+  return result;
 }
 
 export function sum(...numbers) {
@@ -18,5 +26,9 @@ export function sum(...numbers) {
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  let result = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    result += numbers[i];
+  }
+  return result;
 }
