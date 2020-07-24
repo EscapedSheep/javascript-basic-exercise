9969675c88f6e6b2ac1e8a5a728e1d98e98d1ae6
EscapedSheep <sfgmdf001@126.com>
2020-07-24 11:00:25 +0800

    [maida Ge] Completed exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,24 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === null || array === undefined) { throw 'Creating list from undefined array'; }
+  if (array.length === 0) { throw 'Creating list from empty array' }
+
+  class LinkedList {
+    constructor(value, next = null) {
+      this.value = value;
+      this.next = next;
+    }
+  }
+
+  let head = new LinkedList(array[0]);
+  let result = head;
+  let i = 1;
+  while (i < array.length) {
+    head.next = new LinkedList(array[i]);
+    head = head.next;
+    i += 1;
+  }
+  return result;
+  
 }
