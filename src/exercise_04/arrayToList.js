58d89e40e5870be87ee7ea3f8fb60aa184df79b0
EscapedSheep <sfgmdf001@126.com>
2020-07-24 11:35:53 +0800

    [maida Ge] style:change code style of throw error

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,8 +6,8 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  if (array === null || array === undefined) { throw 'Creating list from undefined array'; }
-  if (array.length === 0) { throw 'Creating list from empty array'; }
+  if (array === null || array === undefined) { throw new Error('Creating list from undefined array'); }
+  if (array.length === 0) { throw new Error('Creating list from empty array'); }
 
   class LinkedList {
     constructor(value, next = null) {
